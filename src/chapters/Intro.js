a0a9d107e3990b42db1c2bc2adf1fe84da527826
Lucas Bateson <lucas.bateson@gmail.com>
2023-09-01 19:33:42 +0200

    Intro (Incomplete)

--- src/chapters/Intro.js
+++ src/chapters/Intro.js
@@ -1,8 +1,25 @@
-
 import React from 'react';
 
 function Intro() {
-  return <div><h1>Intro: What is Web Development?</h1></div>;
+  return (
+    <div className="container">
+      <h1>Intro: What is Web Development?</h1>
+      <section className="content-container">
+        <p>Web development is the process of building and maintaining websites and web applications. It involves various aspects of creating web content that users can access through the internet.</p>
+        
+        <h3>Brief History</h3>
+        <p>The history of web development dates back to the early days of the internet. Tim Berners-Lee is credited with inventing the World Wide Web in 1989, which laid the foundation for the web development field. Over the years, web technologies have evolved, leading to the dynamic and interactive websites we see today.</p>
+        
+        <h3>Front-end vs Back-end</h3>
+        <p>Web development is divided into two main categories: front-end and back-end. Front-end development focuses on the user interface and user experience of a website. It involves creating the visual elements that users interact with directly. Back-end development deals with the server-side of things, managing databases, server configuration, and handling data processing.</p>
+        
+        <h3>Career Options</h3>
+        <p>Web development offers a range of career opportunities. Front-end developers work with HTML, CSS, and JavaScript to create visually appealing and interactive user interfaces. Back-end developers handle server-side programming languages, databases, and server management. Full-stack developers have expertise in both front-end and back-end development, making them versatile in building complete web applications.</p>
+        
+        <p>Other roles in web development include UI/UX designers who focus on creating user-centered designs, and web development managers who oversee projects and teams. With the ever-growing demand for online presence, web development continues to be a valuable and rewarding field.</p>
+      </section>
+    </div>
+  );
 }
 
 export default Intro;
